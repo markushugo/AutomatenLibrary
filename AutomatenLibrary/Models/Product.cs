bd0226932ead03bf19723c12047dfbfded549f77
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-08-27 10:13:53 +0200

    hej

--- AutomatenLibrary/Models/Product.cs
+++ AutomatenLibrary/Models/Product.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace AutomatenLibrary.Models
 {
-    internal class Product
+    public class Product
     {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Puffs { get; set; }
+        public string Brand { get; set; }
+
+        public Product(string id, string name, double price, int puffs, string brand)
+        {
+            ID = id;
+            Name = name;
+            Price = price;
+            Puffs = puffs;
+            Brand = brand;
+        }
     }
 }
